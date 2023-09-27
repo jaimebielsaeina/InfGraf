97e8674b698bfdafd6f1a83aeb5338ddce603716
Carlos Mayo <799083@unizar.es>
2023-09-28 01:40:02 +0200

    Equialization done

--- tone_mapping.cpp
+++ tone_mapping.cpp
@@ -10,8 +10,7 @@ void clamping(double &r, double &g, double &b){
     if (b > 1) b = 1;
 }
 
-
-void transform(string inFile, string outFile) {
+void transform_clamping(string inFile, string outFile) {
     int w, h;
     double m, c;
     bool still = true;
@@ -27,7 +26,6 @@ void transform(string inFile, string outFile) {
     string line;
     while (still) {
         getline(ifs, line);
-        cout << line.substr(0, 5) << endl;
         if (line[0] != '#') {
             still = false;
         } else if (line.substr(0, 5) == "#MAX=") {
@@ -57,9 +55,6 @@ void transform(string inFile, string outFile) {
             r = r * m / c;
             g = g * m / c;
             b = b * m / c;
-            if(i == 0 && j == 0){
-                cout << r << " " << g << " " << b << endl;
-            }
 
             clamping(r, g, b);
             
@@ -76,6 +71,105 @@ void transform(string inFile, string outFile) {
 
 }
 
+void equalization(double &r, double &g, double &b, double max){
+    r /= max;
+    g /= max;
+    b /= max;
+}
+
+void transform_equalization(string inFile, string outFile){
+    int w, h;
+    int counter = 3;
+    double m, c;
+    bool still = true;
+    // reads a ppm file
+    ifstream ifs(inFile);
+    // writes a ppm file
+    ofstream ofs(outFile);
+    // reads the header
+    string header;
+    getline(ifs, header);
+    ofs << header << endl;
+    // lee la siguiente linea; si es un comentario, lo ignora a no ser que sea #MAX
+    string line;
+    while (still) {
+        getline(ifs, line);
+        if (line[0] != '#') {
+            still = false;
+        } else if (line.substr(0, 5) == "#MAX=") {
+            ofs << "#MAX=1\n";
+            m = stod(line.substr(5, -1));
+            counter++;
+        } else {
+            ofs << line << endl;
+            counter++;
+        }
+    }
+    // lee el ancho y el alto
+    w = stoi(line.substr(0, line.find(' ')));
+    h = stoi(line.substr(line.find(' ') + 1, -1));
+
+    ofs << w << " " << h << endl;
+    // lee el valor de c
+    getline(ifs, line);
+    c = stod(line);
+
+    ofs << 255 << endl;
+
+    double r, g, b;
+    double max = 0;
+    // lee el valor de cada pixel para encontrar el máximo
+    for (int i = 0; i < h; i++){
+        for (int j = 0; j < w; j++){
+            ifs >> r >> g >> b;
+            r = r * m / c;
+            g = g * m / c;
+            b = b * m / c;
+            if(r > max) max = r;
+            if(g > max) max = g;
+            if(b > max) max = b;
+        }
+    }
+
+    cout << max << endl;
+    // Deja de asociarse con el fichero
+    ifs.close();
+    // Reseteamos el ifstream
+    ifs.open(inFile);
+
+    // Saltamos las lineas previas a los píxeles
+    for (int i = 0; i < counter; i++){
+        getline(ifs, line);
+    }
+
+    // Aplicamos equalization con el valor máximo obtenido
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            ifs >> r >> g >> b;
+
+            r = r * m / c;
+            g = g * m / c;
+            b = b * m / c;
+            
+            equalization(r, g, b, max);
+            
+            r *= 255;
+            g *= 255;
+            b *= 255;
+            
+
+            ofs << fixed << (int)r << " " << (int)g << " "  << (int)b << "     ";
+        }
+        ofs << endl;
+    }
+}
+
+
 int main(int argc, char* argv[]){
-    transform(argv[1], argv[2]);
+    string opcion = argv[1];
+    if(opcion == "clamping"){
+        transform_clamping(argv[2], argv[3]);
+    } else if(opcion == "equalization"){
+        transform_equalization(argv[2], argv[3]);
+    }
 }
